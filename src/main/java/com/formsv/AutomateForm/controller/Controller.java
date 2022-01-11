99a357da847c4ca6a23f5469c0ecbded89533328
Nirbhay Singh <benderthor@gmail.com>
2022-01-11 21:37:03 +0530

    Added some Create Api

--- src/main/java/com/formsv/AutomateForm/controller/Controller.java
+++ src/main/java/com/formsv/AutomateForm/controller/Controller.java
@@ -17,15 +17,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import javax.management.modelmbean.RequiredModelMBean;
-import java.io.IOException;
-import java.util.Base64;
 import java.util.List;
-import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 public class Controller {
