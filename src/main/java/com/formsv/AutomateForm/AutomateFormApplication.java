c9d1776221624c083b4a95895386ea57dd280248
Nirbhay Singh <benderthor@gmail.com>
2022-01-13 20:03:17 +0530

    Cross Origin set to *

--- src/main/java/com/formsv/AutomateForm/AutomateFormApplication.java
+++ src/main/java/com/formsv/AutomateForm/AutomateFormApplication.java
@@ -2,7 +2,9 @@ package com.formsv.AutomateForm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*")
 @SpringBootApplication
 public class AutomateFormApplication {
 
