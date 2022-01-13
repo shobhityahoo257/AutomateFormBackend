a02ac2e94a4e51f01a009f1a7ad69f80c8656f8c
Nirbhay Singh <benderthor@gmail.com>
2022-01-13 10:32:13 +0530

    Added some Create Api

--- src/main/java/com/formsv/AutomateForm/responseModel/RequiredDocumentResponse.java
+++ src/main/java/com/formsv/AutomateForm/responseModel/RequiredDocumentResponse.java
@@ -6,10 +6,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @AllArgsConstructor
 @Getter
 @Setter
 @NoArgsConstructor
 public class RequiredDocumentResponse {
+    private String userId;
+    private String formId;
+    private List<Document> doc;
 
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    class Document{
+        private String documentId;
+        private String documentName;
+        private boolean uploadedByUser;
+    }
 }
+
+
+
+
+
