aa734d1fe689f6b664580f26bd88a10ac4c160aa
shobhitverma <shobhit.verma@telekom-digital.com>
2023-11-05 10:19:32 +0530

    Adding Submit Transaction API

--- src/main/java/com/formsv/AutomateForm/controller/Controller.java
+++ src/main/java/com/formsv/AutomateForm/controller/Controller.java
@@ -136,10 +136,13 @@ This is used to add Required Documents for a form
 
     @PostMapping("/submitTransaction")
     public ResponseEntity submitTransaction(@RequestBody(required = true) Transaction transaction) throws Exception {
+        System.out.println("WITHIN SUBMIT TRANSACTION");
+        System.out.println("TRANSACTION STATUS IS "+ transaction.getTransactionStatus());
         if(transaction.getTransactionStatus().equals("SUCCESS")) {
            // add Retry Framework Here
             applyForm(transaction.getFormId(), transaction.getUserId());
         }
+        System.out.println("FULL TRANSATION IS"+transaction.toString());
        return transactionsService.createTransaction(transaction);
     }
 
